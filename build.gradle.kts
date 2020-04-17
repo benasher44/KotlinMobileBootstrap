873bc2c3860bfa4cbf9eacea4166dbc2e8671037
Ben Asher <benasher44@gmail.com>
2020-04-17 07:30:19 -0700

    Upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask
 import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
 
 plugins {
-    kotlin("multiplatform") version "1.3.70"
+    kotlin("multiplatform") version "1.3.72"
     id("maven-publish")
     id("com.android.library") version "3.6.1"
 }
